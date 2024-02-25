c7857413af53f7836e47aba065de88d4ffeb66d3
mahmoudxyz <mahmoudahmedxyz@gmail.com>
2024-02-25 18:24:38 +0200

    enable CrossOrigin

--- src/main/java/com/jazaredu/jazaredubackendopen/controllers/TrackController.java
+++ src/main/java/com/jazaredu/jazaredubackendopen/controllers/TrackController.java
@@ -5,10 +5,12 @@ import com.jazaredu.jazaredubackendopen.repository.TrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @Controller
+@CrossOrigin
 public class TrackController {
 
     @Autowired
